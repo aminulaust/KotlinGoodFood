0bfe1eb124c35116122087e738ab4001d66a82b4
Aminul <aminulaust@gmail.com>
2017-08-25 01:15:26 +0600

    first commit

---
+++ app/src/main/java/aminulaust/com/goodfood/Food.kt
@@ -0,0 +1,16 @@
+package aminulaust.com.goodfood
+
+/**
+ * Created by Raju on 8/25/17.
+ */
+
+class  Food {
+    var name:String? =null
+    var des:String? =null
+    var image:Int? =null
+    constructor(name:String,des:String,image:Int){
+        this.name=name
+        this.des=des
+        this.image=image
+    }
+}
